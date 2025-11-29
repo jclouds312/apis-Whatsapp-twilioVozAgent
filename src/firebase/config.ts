67934ea80f77ff8b95e01aabdbf5215b785ba6af
administrador aplicativo <aaplicativo676@gmail.com>
2025-11-29 20:51:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3652287884-786f3",
+  "appId": "1:634131812118:web:8c4241d7748b819252ca37",
+  "apiKey": "AIzaSyDrB3cCBQvJlgaS50Rd8JzlHO86pZWVHKU",
+  "authDomain": "studio-3652287884-786f3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "634131812118"
+};
