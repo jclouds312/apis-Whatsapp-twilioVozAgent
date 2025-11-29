4ede3d08726ba8891adc2f8473a8d92ca0266d26
administrador aplicativo <aaplicativo676@gmail.com>
2025-11-29 17:30:42 +0000

    resolver errores para dedpligue en vercel dejar todo listo nueva versión

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,15 +1,15 @@
 // Inspired by: https://github.com/dillionverma/llm.report/blob/main/components/resizable.tsx
 "use client"
 
-import { PanelGroup as ResizablePanelGroupPrimitive, Panel as ResizablePanelPrimitive, PanelResizeHandle as ResizableHandlePrimitive } from "react-resizable-panels"
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 const ResizablePanelGroup = ({
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePanelGroupPrimitive>) => (
-  <ResizablePanelGroupPrimitive
+}: React.ComponentProps<typeof PanelGroup>) => (
+  <PanelGroup
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
@@ -18,16 +18,16 @@ const ResizablePanelGroup = ({
   />
 )
 
-const ResizablePanel = ResizablePanelPrimitive
+const ResizablePanel = Panel
 
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof ResizableHandlePrimitive> & {
+}: React.ComponentProps<typeof PanelResizeHandle> & {
   withHandle?: boolean
 }) => (
-  <ResizableHandlePrimitive
+  <PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -53,7 +53,7 @@ const ResizableHandle = ({
         </svg>
       </div>
     )}
-  </ResizableHandlePrimitive>
+  </PanelResizeHandle>
 )
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
