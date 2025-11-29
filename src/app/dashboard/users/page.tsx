c77c0c0b7edab79a0f76b1dd6603f7b78199b0b9
administrador aplicativo <aaplicativo676@gmail.com>
2025-11-29 17:59:31 +0000

    12:56:38.078 Running build in Washington, D.C., USA (East) – iad1 12:56:

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Header } from "@/components/dashboard/header";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
