0cf3e1062054d0d45926dd8be81cf4292495c38c
universalserviciostec <universalserviciostec@gmail.com>
2025-12-12 22:23:01 +0000

    ok

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,11 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "@shared/schema";
+import "dotenv/config";
 
-export const sqlite = new Database("sqlite.db");
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL must be set. Did you forget to provision a database?");
+}
+
+export const sqlite = new Database(process.env.DATABASE_URL);
 export const db = drizzle(sqlite, { schema });
